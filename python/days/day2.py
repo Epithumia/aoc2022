51fab8e851ab9efcae04a7d137f4eb85d3a68624
Rafael Lopez <lopez@airelle.info>
2022-12-02 07:54:45 +0100

    Jour 2 (Python)

--- python/days/day2.py
+++ python/days/day2.py
@@ -1,4 +1,62 @@
 def day2():
-    with open('input/input2.txt', 'r') as f:
-        data = f.read()
-    print(data)
+    with open('../input/input2.txt', 'r') as f:
+        data = f.read().split('\n')
+        round1 = 0
+        round2 = 0
+        for row in data:
+            if len(row) > 0:
+                m = row.split(' ')
+                round1 += score(m[0], m[1])
+                round2 += strategize(m[0], m[1])
+    print('Part 1: ', round1)
+    print('Part 2: ', round2)
+
+
+def score(p1: str, p2: str) -> int:
+    if p1 == 'A':
+        if p2 == 'X':
+            return 1 + 3
+        if p2 == 'Y':
+            return 2 + 6
+        if p2 == 'Z':
+            return 3 + 0
+    if p1 == 'B':
+        if p2 == 'X':
+            return 1 + 0
+        if p2 == 'Y':
+            return 2 + 3
+        if p2 == 'Z':
+            return 3 + 6
+    if p1 == 'C':
+        if p2 == 'X':
+            return 1 + 6
+        if p2 == 'Y':
+            return 2 + 0
+        if p2 == 'Z':
+            return 3 + 3
+    return 0
+
+
+def strategize(p1: str, p2: str) -> int:
+    if p1 == 'A':
+        if p2 == 'X':
+            return 3 + 0
+        if p2 == 'Y':
+            return 1 + 3
+        if p2 == 'Z':
+            return 2 + 6
+    if p1 == 'B':
+        if p2 == 'X':
+            return 1 + 0
+        if p2 == 'Y':
+            return 2 + 3
+        if p2 == 'Z':
+            return 3 + 6
+    if p1 == 'C':
+        if p2 == 'X':
+            return 2 + 0
+        if p2 == 'Y':
+            return 3 + 3
+        if p2 == 'Z':
+            return 1 + 6
+    return 0
