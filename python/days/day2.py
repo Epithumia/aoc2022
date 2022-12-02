aea275670c5babcf20c7c18356271d54c1624934
Rafael Lopez <lopez@airelle.info>
2022-12-02 08:04:53 +0100

    Jour 2 (Python nettoyé)

--- python/days/day2.py
+++ python/days/day2.py
@@ -13,50 +13,20 @@ def day2():
 
 
 def score(p1: str, p2: str) -> int:
-    if p1 == 'A':
-        if p2 == 'X':
-            return 1 + 3
-        if p2 == 'Y':
-            return 2 + 6
-        if p2 == 'Z':
-            return 3 + 0
-    if p1 == 'B':
-        if p2 == 'X':
-            return 1 + 0
-        if p2 == 'Y':
-            return 2 + 3
-        if p2 == 'Z':
-            return 3 + 6
-    if p1 == 'C':
-        if p2 == 'X':
-            return 1 + 6
-        if p2 == 'Y':
-            return 2 + 0
-        if p2 == 'Z':
-            return 3 + 3
+    results = {'A': {'X': 1 + 3, 'Y': 2 + 6, 'Z': 3 + 0},
+               'B': {'X': 1 + 0, 'Y': 2 + 3, 'Z': 3 + 6},
+               'C': {'X': 1 + 6, 'Y': 2 + 0, 'Z': 3 + 3}
+               }
+    if p1 in results and p2 in results[p1]:
+        return results[p1][p2]
     return 0
 
 
 def strategize(p1: str, p2: str) -> int:
-    if p1 == 'A':
-        if p2 == 'X':
-            return 3 + 0
-        if p2 == 'Y':
-            return 1 + 3
-        if p2 == 'Z':
-            return 2 + 6
-    if p1 == 'B':
-        if p2 == 'X':
-            return 1 + 0
-        if p2 == 'Y':
-            return 2 + 3
-        if p2 == 'Z':
-            return 3 + 6
-    if p1 == 'C':
-        if p2 == 'X':
-            return 2 + 0
-        if p2 == 'Y':
-            return 3 + 3
-        if p2 == 'Z':
-            return 1 + 6
+    results = {'A': {'X': 3 + 0, 'Y': 1 + 3, 'Z': 2 + 6},
+               'B': {'X': 1 + 0, 'Y': 2 + 3, 'Z': 3 + 6},
+               'C': {'X': 2 + 0, 'Y': 3 + 3, 'Z': 1 + 6}
+               }
+    if p1 in results and p2 in results[p1]:
+        return results[p1][p2]
     return 0
