e764baebd27c463ca82a074dcfb060b3b2ad9d13
Rafael Lopez <lopez@airelle.info>
2022-12-05 07:26:42 +0100

    Jour 5 (python) reformat

--- python/days/day5.py
+++ python/days/day5.py
@@ -2,7 +2,7 @@ def day5():
     with open('../input/input5.txt', 'r') as f:
         data = f.read().split('\n')[:-1]
 
-    c = [list(reversed([d[4*i+1] if d[4*i+1] != ' ' else None for d in data[:8]])) for i in range(9)]
+    c = [list(reversed([d[4 * i + 1] if d[4 * i + 1] != ' ' else None for d in data[:8]])) for i in range(9)]
     crates9k = [[x for x in row if x is not None] for row in c]
     crates9k1 = [[x for x in row if x is not None] for row in c]
     moves = [(int(row.split(' ')[1]), int(row.split(' ')[3]) - 1, int(row.split(' ')[5]) - 1) for row in data[10:]]
@@ -14,6 +14,3 @@ def day5():
 
     print('Part 1: ', ''.join([c[-1] for c in crates9k]))
     print('Part 2: ', ''.join([c[-1] for c in crates9k1]))
-
-
-
