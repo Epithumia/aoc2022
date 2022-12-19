6b0a0bf4e06bc1d7e16c408da6f34fefc6a40adc
Rafael Lopez <lopez@airelle.info>
2022-12-19 09:40:08 +0100

    Jour 19 (python) cleanup

--- python/aoc2022/days/day19.py
+++ python/aoc2022/days/day19.py
@@ -36,83 +36,71 @@ def day19():
 
     score = 1
 
-    for blueprint in tqdm(blueprints[0:3]):
+    for blueprint in tqdm(blueprints[0:min(3, len(blueprints))]):  # min needed for test case
         s = build(blueprint, 32)
         score *= s
 
     print('Part 2:', score)
 
 
+def init_array(name, duration, var_type):
+    arr = [None]
+    for m in range(1, duration + 1):
+        arr.append(LpVariable(name.format(m=m), 0, None, var_type))
+    return arr
+
+
 def build(blueprint, duration):
     prob = LpProblem("Tuning", LpMaximize)
-    nb_ore_robots = [None]
-    nb_clay_robots = [None]
-    nb_obs_robots = [None]
-    nb_geo_robots = [None]
-    nb_ore_day_start = [None]
-    nb_clay_day_start = [None]
-    nb_obs_day_start = [None]
-    nb_geo_day_start = [None]
-    nb_ore_day_end = [None]
-    nb_clay_day_end = [None]
-    nb_obs_day_end = [None]
-    nb_geo_day_end = [None]
-    for m in range(1, duration + 1):
-        nb_ore_robot = LpVariable(f"ore_robot_{m}", 0, None, LpBinary)
-        nb_ore_robots.append(nb_ore_robot)
-        nb_clay_robot = LpVariable(f"nb_clay_robot_{m}", 0, None, LpBinary)
-        nb_clay_robots.append(nb_clay_robot)
-        nb_obs_robot = LpVariable(f"nb_obs_robot_{m}", 0, None, LpBinary)
-        nb_obs_robots.append(nb_obs_robot)
-        nb_geo_robot = LpVariable(f"nb_geo_robot_{m}", 0, None, LpBinary)
-        nb_geo_robots.append(nb_geo_robot)
-        nb_ore_start = LpVariable(f"nb_ore_start_{m}", 0, None, LpInteger)
-        nb_ore_day_start.append(nb_ore_start)
-        nb_clay_start = LpVariable(f"nb_clay_start_{m}", 0, None, LpInteger)
-        nb_clay_day_start.append(nb_clay_start)
-        nb_obs_start = LpVariable(f"nb_obs_start_{m}", 0, None, LpInteger)
-        nb_obs_day_start.append(nb_obs_start)
-        nb_geo_start = LpVariable(f"nb_geo_start_{m}", 0, None, LpInteger)
-        nb_geo_day_start.append(nb_geo_start)
-        nb_ore_end = LpVariable(f"nb_ore_end_{m}", 0, None, LpInteger)
-        nb_ore_day_end.append(nb_ore_end)
-        nb_clay_end = LpVariable(f"nb_clay_end_{m}", 0, None, LpInteger)
-        nb_clay_day_end.append(nb_clay_end)
-        nb_obs_end = LpVariable(f"nb_obs_end_{m}", 0, None, LpInteger)
-        nb_obs_day_end.append(nb_obs_end)
-        nb_geo_end = LpVariable(f"nb_geo_end_{m}", 0, None, LpInteger)
-        nb_geo_day_end.append(nb_geo_end)
+    ore_robots = init_array("ore_robot_{m}", duration, LpBinary)
+    clay_robots = init_array("clay_robot_{m}", duration, LpBinary)
+    obs_robots = init_array("obs_robot_{m}", duration, LpBinary)
+    geo_robots = init_array("geo_robot_{m}", duration, LpBinary)
+    nb_ore_day_start = init_array("nb_ore_start_{m}", duration, LpInteger)
+    nb_clay_day_start = init_array("nb_clay_start_{m}", duration, LpInteger)
+    nb_obs_day_start = init_array("nb_obs_start_{m}", duration, LpInteger)
+    nb_geo_day_start = init_array("nb_geo_start_{m}", duration, LpInteger)
+    nb_ore_day_end = init_array("nb_ore_end_{m}", duration, LpInteger)
+    nb_clay_day_end = init_array("nb_clay_end_{m}", duration, LpInteger)
+    nb_obs_day_end = init_array("nb_obs_end_{m}", duration, LpInteger)
+    nb_geo_day_end = init_array("nb_geo_end_{m}", duration, LpInteger)
 
     prob += nb_geo_day_end[duration]
     for m in range(1, duration + 1):
         # build at most one robot
-        prob += nb_ore_robots[m] + nb_clay_robots[m] + nb_obs_robots[m] + nb_geo_robots[m] <= 1
+        prob += ore_robots[m] + clay_robots[m] + obs_robots[m] + geo_robots[m] <= 1
 
         if m > 1:
-            # Number of ore on start of day m = nb day before - build cost
-            prob += nb_ore_day_start[m] == nb_ore_day_end[m - 1] - nb_ore_robots[m] * blueprint['ore_robot_ore_cost'] \
-                    - nb_clay_robots[m] * blueprint['clay_robot_ore_cost'] - nb_obs_robots[m] * blueprint[
-                        'obs_robot_ore_cost'] - nb_geo_robots[m] * blueprint['geo_robot_ore_cost']
+            # Number of ore on start of day m = nb day before - build cost, must stay >= 0
+            prob += nb_ore_day_start[m] == nb_ore_day_end[m - 1] - ore_robots[m] * blueprint['ore_robot_ore_cost'] \
+                    - clay_robots[m] * blueprint['clay_robot_ore_cost'] - obs_robots[m] * blueprint[
+                        'obs_robot_ore_cost'] - geo_robots[m] * blueprint['geo_robot_ore_cost']
             prob += nb_ore_day_start[m] >= 0
             # End of the day = start + production
-            prob += nb_ore_day_end[m] == nb_ore_day_start[m] + sum([nb_ore_robots[k] for k in range(1, m)])
+            prob += nb_ore_day_end[m] == nb_ore_day_start[m] + sum([ore_robots[k] for k in range(1, m)])
             # Number of clay
-            prob += nb_clay_day_start[m] == nb_clay_day_end[m - 1] - nb_obs_robots[m] * blueprint[
+            # Start
+            prob += nb_clay_day_start[m] == nb_clay_day_end[m - 1] - obs_robots[m] * blueprint[
                 'obs_robot_clay_cost']
             prob += nb_clay_day_start[m] >= 0
-            prob += nb_clay_day_end[m] == nb_clay_day_start[m] + sum([nb_clay_robots[k] for k in range(1, m)])
+            # End
+            prob += nb_clay_day_end[m] == nb_clay_day_start[m] + sum([clay_robots[k] for k in range(1, m)])
             # Number of obsidian
-            prob += nb_obs_day_start[m] == nb_obs_day_end[m - 1] - nb_geo_robots[m] * blueprint[
+            # Start
+            prob += nb_obs_day_start[m] == nb_obs_day_end[m - 1] - geo_robots[m] * blueprint[
                 'geo_robot_obs_cost']
             prob += nb_obs_day_start[m] >= 0
-            prob += nb_obs_day_end[m] == nb_obs_day_start[m] + sum([nb_obs_robots[k] for k in range(1, m)])
+            # End
+            prob += nb_obs_day_end[m] == nb_obs_day_start[m] + sum([obs_robots[k] for k in range(1, m)])
             # Number of geode
+            # Start
             prob += nb_geo_day_start[m] == nb_geo_day_end[m - 1]
             prob += nb_geo_day_start[m] >= 0
-            prob += nb_geo_day_end[m] == nb_geo_day_start[m] + sum([nb_geo_robots[k] for k in range(1, m)])
+            # End
+            prob += nb_geo_day_end[m] == nb_geo_day_start[m] + sum([geo_robots[k] for k in range(1, m)])
 
     # First robot is an ore robot
-    prob += nb_ore_robots[1] == 1
+    prob += ore_robots[1] == 1
 
     # First day
     prob += nb_ore_day_start[1] == 0
